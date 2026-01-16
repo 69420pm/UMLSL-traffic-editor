24cea8ae544b1f50522b140da91fa714430f70ad
ventolotl <ventolotl@protonmail.com>
2026-01-16 18:12:23 +0100

    improve performance of hchop

--- pse/umlsl_editor/src/query/ast/chop_node.py
+++ pse/umlsl_editor/src/query/ast/chop_node.py
@@ -3,20 +3,33 @@ from pse.umlsl_editor.src.model.view_models.traffic_snapshot import TrafficSnaps
 from pse.umlsl_editor.src.query.ast.ast import View, BinaryNode, Precedence, ASTNode
 from pse.umlsl_editor.src.query.interval import Interval
 
-# Determines in how many pieces we split a given space interval
-H_CHOP_ACCURACY: float = 1000.0
-
+# Sets the number of iterations for the horizontal chopping (at least 1). In each iteration, the new step_size is
+# computed via step_size = prev_step_size / 2.
+H_CHOP_EVAL_ITERATIONS = 5
 
 class HorizontalChopNode(BinaryNode):
     def __init__(self, left: ASTNode, right: ASTNode):
         super().__init__(Precedence.BINARY_CHOP, left, right)
 
     def evaluate(self, traffic_snapshot: TrafficSnapshot, view: View, variable_car_map: dict[str, Car]) -> bool:
-        space_interval = view.space_interval
-        step_size = space_interval.length() / H_CHOP_ACCURACY
+        for i in range(0, H_CHOP_EVAL_ITERATIONS - 1):
+            if self.evaluate_with_step_size(i, traffic_snapshot, view, variable_car_map):
+                return True
 
+        return False
+
+    def evaluate_with_step_size(self, eval_iteration: int, traffic_snapshot: TrafficSnapshot, view: View,
+                                variable_car_map: dict[str, Car]):
+        step_size = self.compute_step_size(eval_iteration)
+
+        space_interval = view.space_interval
         split_value = space_interval.start
+
+        iteration = 0
         while split_value < space_interval.end:
+            if eval_iteration != 0 and self.skip_step(iteration):
+                continue
+
             space_interval1 = Interval(space_interval.start, split_value)
             space_interval2 = Interval(split_value, space_interval.end)
 
@@ -28,8 +41,15 @@ class HorizontalChopNode(BinaryNode):
                 return True
 
             split_value += step_size
+            iteration += 1
 
-        return False
+    def compute_step_size(self, eval_iteration: int) -> float:
+        # uses step_size: 1, 1/2, 1/4, 1/8, ...
+        return 1.0 / pow(2, eval_iteration)
+
+    def skip_step(self, iteration: int) -> bool:
+        # skips every second step (already captured by the previous one because of our choice of 1/2^i)
+        return iteration % 2 == 0
 
     def _format(self, left: str, right: str) -> str:
         return f"{left} \\smallfrown {right}"
