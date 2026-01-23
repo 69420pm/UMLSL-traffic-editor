9f8bdbfb7b95af2d1aa105ee0e0bb5968a35629c
69420pm <420dev69420@gmail.com>
2026-01-23 14:42:11 +0100

    some changes

--- pse/umlsl_editor/src/model/traffic_value_objects/segments/crossing_segment.py
+++ pse/umlsl_editor/src/model/traffic_value_objects/segments/crossing_segment.py
@@ -1,20 +1,8 @@
 from dataclasses import dataclass
 
-<<<<<<<< HEAD:pse/umlsl_editor/src/model/value_objects/segments/crossing_segment.py
-<<<<<<<< HEAD:pse/umlsl_editor/src/core/value_objects/segments/crossing_segment.py
-from pse.umlsl_editor.src.core.value_objects.lane import Lane
-from pse.umlsl_editor.src.core.value_objects.position import Position
-from pse.umlsl_editor.src.core.value_objects.segments.segment import Segment
-========
-from pse.umlsl_editor.src.model.value_objects.lane import Lane
-from pse.umlsl_editor.src.model.value_objects.position import Position
-from pse.umlsl_editor.src.model.value_objects.segments.segment import Segment
->>>>>>>> 83e486a70109dc9a73cb47ad2fbf2eaa9e93b535:pse/umlsl_editor/src/model/value_objects/segments/crossing_segment.py
-========
 from pse.umlsl_editor.src.model.traffic_value_objects.lane import Lane
 from pse.umlsl_editor.src.model.traffic_value_objects.position import Position
 from pse.umlsl_editor.src.model.traffic_value_objects.segments.segment import Segment
->>>>>>>> f5aa8be893b6cdb275c41e175b4b8127d5c941ae:pse/umlsl_editor/src/model/traffic_value_objects/segments/crossing_segment.py
 
 
 @dataclass(frozen=True)
