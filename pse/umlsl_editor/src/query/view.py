c04053a1a3f7eed7a3fe08234f61d6c8075ad282
ventolotl <ventolotl@protonmail.com>
2026-02-05 19:29:02 +0100

    fix python not telling me about invalid code

--- pse/umlsl_editor/src/query/view.py
+++ pse/umlsl_editor/src/query/view.py
@@ -1,10 +1,9 @@
-from pse.umlsl_editor.src.model.entities.car import Car
 from pse.umlsl_editor.src.model.traffic_value_objects.segments.segment import VirtualLane
 from pse.umlsl_editor.src.model.interval import Interval
 
 
 class View:
-    def __init__(self, virtual_lanes: list[VirtualLane], space_interval: Interval, car: Car):
+    def __init__(self, virtual_lanes: list[VirtualLane], space_interval: Interval, car: 'Car'):
         self.virtual_lanes = virtual_lanes
         self.space_interval = space_interval
         self.car = car
