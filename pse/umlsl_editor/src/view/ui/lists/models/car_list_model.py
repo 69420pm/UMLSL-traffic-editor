8a8d58f59f709ee675571cfdc03ef8ef30bb51d4
luazars <werbungmaef@gmail.com>
2026-02-07 20:35:25 +0100

    fixed help dialog syntax, fixed latex preview dont scale correctly

--- pse/umlsl_editor/src/view/ui/lists/models/car_list_model.py
+++ pse/umlsl_editor/src/view/ui/lists/models/car_list_model.py
@@ -89,7 +89,7 @@ class CarModel(EntityModel):
         elif role == CarModel.ColorRole:
             return str(car.color)
         elif role == CarModel.ValueRole:
-            return f"R: {self._application_controller.data_controller.get_road_by_uid(car.lane.road_uid).name} L: {car.lane.lane_index}"
+            return f"R: {self._application_controller.data_controller.get_road_by_uid(car.lane.road_uid).name} L: {car.lane.get_name()}"
 
         return None
 
