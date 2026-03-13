72fba409bc4578d0b88e7207ee27494aa8ca6910
ventolotl <ventolotl@protonmail.com>
2026-03-13 15:39:16 +0100

    add back binary-search like hchop

--- pse/umlsl_editor/src/query/ast/chop_node.py
+++ pse/umlsl_editor/src/query/ast/chop_node.py
@@ -1,31 +1,52 @@
+from math import ceil
+
 from pse.umlsl_editor.src.model.domain_models.traffic_snapshot_model import TrafficSnapshotModel
 from pse.umlsl_editor.src.model.entities.car import Car
 from pse.umlsl_editor.src.query.ast.ast import View, BinaryNode, Precedence, ASTNode
 from pse.umlsl_editor.src.model.interval import Interval
 
-# Sets the number of iterations for the horizontal chopping (at least 1). In each iteration, the new step_size is
-# computed via step_size = prev_step_size / 2.
-H_CHOP_EVAL_ITERATIONS = 4
+SMALLEST_STEP_SIZE = 0.5
+MAX_ITERATIONS = 4
+
 
 class HorizontalChopNode(BinaryNode):
     def __init__(self, left: ASTNode, right: ASTNode):
         super().__init__(Precedence.BINARY_CHOP, left, right)
 
     def evaluate(self, traffic_snapshot: TrafficSnapshotModel, view: View, variable_car_map: dict[str, Car]) -> bool:
-        # todo: split pieces in half on every iteration
-        step_size = 0.5  # self.compute_step_size(eval_iteration)
-        iteration = 0
+        horizon_length = view.horizon.length()
 
+        # how many iterations we need to compute the chopping
+        total_steps = horizon_length / SMALLEST_STEP_SIZE
+        step_size = ceil(total_steps / MAX_ITERATIONS)
+
+        current_step = 0
+        while current_step < total_steps:
+            eval_step_size = max(SMALLEST_STEP_SIZE, horizon_length / (1 + current_step))
+
+            if self.evaluate_with_step_size(view, traffic_snapshot, variable_car_map, eval_step_size):
+                return True
+
+            if eval_step_size == SMALLEST_STEP_SIZE:
+                break
+
+            current_step += step_size
+
+        return False
+
+    def evaluate_with_step_size(self, view: View, traffic_snapshot: TrafficSnapshotModel,
+                                variable_car_map: dict[str, Car], step_size: float):
+        horizon = view.horizon
         split_value = view.horizon.start
-        space_interval = view.horizon
-        while split_value < space_interval.end - 0.001:
-            left_horizon = Interval(space_interval.start, split_value)
+
+        while split_value < horizon.end - 0.001:
+            left_horizon = Interval(horizon.start, split_value)
             left_view = View(view.virtual_lanes, left_horizon, view.car)
             left_eval = self._left.evaluate(traffic_snapshot, left_view, variable_car_map)
 
             # if the lhs is false, we can skip the computation of the rhs
             if left_eval:
-                right_horizon = Interval(split_value, space_interval.end)
+                right_horizon = Interval(split_value, horizon.end)
                 right_view = View(view.virtual_lanes, right_horizon, view.car)
 
                 right_eval = self._right.evaluate(traffic_snapshot, right_view, variable_car_map)
@@ -35,14 +56,6 @@ class HorizontalChopNode(BinaryNode):
             split_value += step_size
 
         return False
-
-    def compute_step_size(self, eval_iteration: int) -> float:
-        # uses step_size: 1, 1/2, 1/4, 1/8, ...
-        return 1.0 / pow(2, eval_iteration)
-
-    def skip_step(self, iteration: int) -> bool:
-        # skips every second step (already captured by the previous one because of our choice of 1/2^i)
-        return iteration % 2 == 0
 
     def _format(self, left: str, right: str) -> str:
         return f"{left} \\frown {right}"
