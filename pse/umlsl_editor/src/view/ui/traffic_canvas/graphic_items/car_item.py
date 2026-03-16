2ff275b7fef240e9af274268fcfb4c5cd48c3051
ventolotl <ventolotl@protonmail.com>
2026-03-16 03:17:17 +0100

    visualize horizon

--- pse/umlsl_editor/src/view/ui/traffic_canvas/graphic_items/car_item.py
+++ pse/umlsl_editor/src/view/ui/traffic_canvas/graphic_items/car_item.py
@@ -79,12 +79,17 @@ class CarItem(SelectableGraphicsItem):
         self._clear_segments()
 
         self._add_segments(
-            self._car.environment.reserved_lanes + self._car.environment.reserved_crossings,
+            self._car.environment.path_segment_intervals,
             self._car,
             False
         )
         self._add_segments(
-            self._car.environment.claimed_lanes + self._car.environment.claimed_crossings,
+            self._car.environment.reserved,
+            self._car,
+            False
+        )
+        self._add_segments(
+            self._car.environment.claimed,
             self._car,
             True
         )
@@ -136,12 +141,6 @@ class CarItem(SelectableGraphicsItem):
         self._body_pen = QPen(border_color, CarItemStyle.PEN_WIDTH)
 
     def on_selection_changed(self, is_selected: bool) -> None:
-        if is_selected:
-            self.application_controller.get_traffic_snapshot_reader().debug_get_segments().clear()
-            for seg in self._car.environment.path_segment_intervals:
-                self.application_controller.get_traffic_snapshot_reader().debug_get_segments()[
-                    seg.segment.uid] = seg.segment
-
         self._update_styles()
         self.update()
 
