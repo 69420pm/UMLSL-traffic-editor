524b353616e7db5c4ba2f158abdf26bf03b20483
ventolotl <ventolotl@protonmail.com>
2026-03-16 23:35:27 +0100

    allow infinite spaces between comparison operators in lexer

--- pse/umlsl_editor/src/query/lexer.py
+++ pse/umlsl_editor/src/query/lexer.py
@@ -4,15 +4,15 @@ from enum import Enum
 
 
 class TokenType(Enum):
-    L_PAREN = "("
-    R_PAREN = ")"
-    L_CURLY = "{"
-    R_CURLY = "}"
+    L_PAREN = "\\("
+    R_PAREN = "\\)"
+    L_CURLY = "\\{"
+    R_CURLY = "\\}"
     COLON = ":"
-    HORIZON_GE = "l>="
-    HORIZON_LE = "l<="
-    HORIZON_LT = "l<"
-    HORIZON_GT = "l>"
+    HORIZON_GE = "l\\s*>="
+    HORIZON_LE = "l\\s*<="
+    HORIZON_LT = "l\\s*<"
+    HORIZON_GT = "l\\s*>"
     IMPLIES = "=>"
     LESS_THAN = "<"
     GREATER_THAN = ">"
@@ -146,12 +146,12 @@ class Lexer:
         self._input = text
 
     def tokenize(self) -> list[Token]:
-        input = self._input
+        query_input = self._input
 
         token_patterns = []
         for t in TokenType:
             if t is not TokenType.LITERAL:
-                pattern = f"(?P<{t.name}>{re.escape(t.value)})"
+                pattern = f"(?P<{t.name}>{t.value})"
                 token_patterns.append(pattern)
 
         master_pattern = re.compile("|".join(token_patterns))
@@ -159,12 +159,12 @@ class Lexer:
         tokens = []
         last_pos = 0
 
-        for match in master_pattern.finditer(input):
+        for match in master_pattern.finditer(query_input):
             start = match.start()
             if start > last_pos:
                 literal_start = last_pos
                 literal_end = match.start()
-                literal_text = input[literal_start:literal_end].strip()
+                literal_text = query_input[literal_start:literal_end].strip()
 
                 if len(literal_text) != 0 and literal_text != " ":
                     tokens.append(Literal(literal_text, literal_start, literal_end))
@@ -176,9 +176,9 @@ class Lexer:
 
             last_pos = end
 
-        if last_pos < len(input):
-            literal_text = input[last_pos:].strip()
+        if last_pos < len(query_input):
+            literal_text = query_input[last_pos:].strip()
             if len(literal_text) != 0 and literal_text != " ":
-                tokens.append(Literal(literal_text, last_pos, len(input)))
+                tokens.append(Literal(literal_text, last_pos, len(query_input)))
 
         return tokens
